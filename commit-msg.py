4811e01e7d05b3a3b3d2619c52c82cd1332af8e2
Craig <craig@harmony-institute.org>
2015-10-05 15:12:05 -0400

    Fixed error that would not abort on an empty commit message

--- commit-msg.py
+++ commit-msg.py
@@ -11,18 +11,31 @@ from subprocess import check_output
 # Collect the parameters
 commit_file = sys.argv[1]
 
+message = ""
+
 # open and read the file
 file = open(commit_file, 'r+')
-message= file.read()
+line = file.readline()
+while line:
+    # strip out comments as we don't want to count them (they'll be removed anyway).
+    line = line.strip()
+    if line[0] != '#':
+        message += line
 
+    line = file.readline()
+
+# with comments striped out, if the message is empty, abort commit
 if message == "":
+    file.close()
     exit(1)
 
 # find the ID from out branch name
 branch = check_output(['git', 'symbolic-ref', '--short', 'HEAD']).strip()
 regex = "-([0-9]+)[-]*"
 match = re.search(regex, branch)
+# If there is no match, then just exit and proceed as normal
 if not match:
+    file.close()
     exit(0)
 
 url = "https://www.pivotaltracker.com/story/show/{}".format(match.group(1))
@@ -30,7 +43,7 @@ url = "https://www.pivotaltracker.com/story/show/{}".format(match.group(1))
 # don't add the url twice (for amending)
 match = re.search(url, message)
 if not match:
-    message = message + "\n" + url
+    message += "\n" + url
     file.seek(0,0)
     file.write(message)
     file.close()
