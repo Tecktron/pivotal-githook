c99b59755e12d881b155f9f4cf40f67c565a7e47
Craig <craig@harmony-institute.org>
2015-10-07 17:35:23 -0400

    Updated code to handle amend messages

--- commit-msg.py
+++ commit-msg.py
@@ -8,25 +8,35 @@
 import sys, re
 from subprocess import check_output
 
-# Collect the parameters
-commit_file = sys.argv[1]
 
-message = ""
+def get_message(file):
+    """
+    Retrieves the message of the file without lines that contain comments.
+    :param file:  file
+    :return: the message of the text.
+    """
+    text = ""
+    line = file.readline()
+    while line:
+        # strip out comments as we don't want to count them (they'll be removed anyway).
+        line = line.strip()
+        if line and line[0] != '#':
+            text += line
+
+        line = file.readline()
+
+    return text
+
+if not sys.argv[1]:
+    exit(1)
 
 # open and read the file
-file = open(commit_file, 'r+')
-line = file.readline()
-while line:
-    # strip out comments as we don't want to count them (they'll be removed anyway).
-    line = line.strip()
-    if line[0] != '#':
-        message += line
-
-    line = file.readline()
+commit_file = open(sys.argv[1], 'r+')
+message = get_message(commit_file)
 
 # with comments striped out, if the message is empty, abort commit
 if message == "":
-    file.close()
+    commit_file.close()
     exit(1)
 
 # find the ID from out branch name
@@ -35,7 +45,7 @@ regex = "-([0-9]+)[-]*"
 match = re.search(regex, branch)
 # If there is no match, then just exit and proceed as normal
 if not match:
-    file.close()
+    commit_file.close()
     exit(0)
 
 url = "https://www.pivotaltracker.com/story/show/{}".format(match.group(1))
@@ -44,8 +54,8 @@ url = "https://www.pivotaltracker.com/story/show/{}".format(match.group(1))
 match = re.search(url, message)
 if not match:
     message += "\n" + url
-    file.seek(0,0)
-    file.write(message)
-    file.close()
+    commit_file.seek(0,0)
+    commit_file.write(message)
 
+commit_file.close()
 exit(0)
